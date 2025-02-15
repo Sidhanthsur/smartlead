2beeda75b7f5890fdc984c25ad449c0fe541a166
Sidhanthsur <sidhanth.surana@aspireapp.com>
2025-02-15 11:27:41 +0530

    feat: add SLSearchBar component and integrate search functionality in EmailCampaignsView

---
+++ src/components/SLSearchBar.vue
@@ -0,0 +1,58 @@
+<script setup>
+import { ref } from 'vue'
+import searchSVG from '@/assets/Union.svg'
+
+const searchQuery = ref('')
+const emit = defineEmits(['search'])
+
+const handleInput = (event) => {
+  searchQuery.value = event.target.value
+  emit('search', searchQuery.value)
+}
+</script>
+
+<template>
+  <div class="search-bar">
+    <div class="search-icon">
+      <img :src="searchSVG" alt="search" />
+    </div>
+    <input
+      type="text"
+      class="search-input"
+      placeholder="Search campaigns"
+      :value="searchQuery"
+      @input="handleInput"
+    />
+  </div>
+</template>
+
+<style scoped>
+.search-bar {
+  display: flex;
+  align-items: center;
+  background: #fafaff;
+  border-radius: 6px;
+  padding: 8px 12px;
+  width: 240px;
+}
+
+.search-icon {
+  display: flex;
+  align-items: center;
+  margin-right: 8px;
+}
+
+.search-input {
+  border: none;
+  background: transparent;
+  outline: none;
+  font-family: 'DM Sans', sans-serif;
+  font-size: 14px;
+  color: #282b42;
+  width: 100%;
+}
+
+.search-input::placeholder {
+  color: #686b8a;
+}
+</style>
--- src/views/EmailCampaignsView.vue
+++ src/views/EmailCampaignsView.vue
@@ -1,9 +1,50 @@
 <script setup>
+import { ref } from 'vue'
 import { campaigns } from '@/constants/campaigns'
 import SLCampaignItem from '@/components/SLCampaignItem.vue'
+import SLSearchBar from '@/components/SLSearchBar.vue'
+const filteredCampaigns = ref(campaigns)
+
+const handleSearch = (query) => {
+  filteredCampaigns.value = campaigns.filter((campaign) =>
+    campaign.title.toLowerCase().includes(query.toLowerCase())
+  )
+}
 </script>
 <template>
-  <div>
-    <SLCampaignItem v-for="campaign in campaigns" :key="campaign.id" :campaign="campaign" />
+  <div class="email-campaigns">
+    <div class="email-campaigns-header">
+      <span class="email-campaigns__header-title"
+        >All Campaigns ({{ filteredCampaigns.length }})</span
+      >
+      <div>
+        <SLSearchBar @search="handleSearch" />
+      </div>
+    </div>
+    <SLCampaignItem v-for="campaign in filteredCampaigns" :key="campaign.id" :campaign="campaign" />
   </div>
 </template>
+<style scoped>
+.email-campaigns {
+  width: 100%;
+  flex: 1;
+}
+.email-campaigns-header {
+  height: 3.75rem;
+  background: white;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1.25rem;
+}
+
+.email-campaigns__header-title {
+  font-family: DM Sans;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 20.83px;
+  letter-spacing: 0%;
+  color: #282b42;
+}
+</style>
