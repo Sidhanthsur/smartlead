df746191cb50935fd0a171932d88aaaf68b735e6
Sidhanthsur <sidhanth.surana@aspireapp.com>
2025-02-15 12:51:57 +0530

    feat: add UserHeader component with banner and SVG integration

---
+++ src/components/UserHeader.vue
@@ -0,0 +1,18 @@
+<script setup>
+import smartSvg from '@/assets/smart.svg'
+</script>
+<template>
+  <div class="banner">
+    <img :src="smartSvg" />
+  </div>
+</template>
+<style>
+.banner {
+  width: 100%;
+  background: #00056a;
+  height: 3.125rem;
+  display: flex;
+  align-items: center;
+  padding: 1.5rem 1.125rem;
+}
+</style>
