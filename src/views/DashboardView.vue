95bfdebc405bf96fd41cdb4ad68bda8fdd5b8e84
Sidhanthsur <sidhanth.surana@aspireapp.com>
2025-02-15 10:16:10 +0530

    refactor: improve code formatting and remove unnecessary line breaks in DashboardView

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,48 +1,48 @@
 <script setup>
-import SLSideMenu from '@/components/SLSideMenu.vue';
-import EmailSVG from '@/assets/email.svg';
-import LeadsSVG from '@/assets/leads.svg';
-import CampaignsSVG from '@/assets/campaigns.svg';
-import router from '@/router';
-import SlackSVG from '@/assets/slack.svg';
-import PlaySVG from '@/assets/play.svg';
-
+import SLSideMenu from '@/components/SLSideMenu.vue'
+import EmailSVG from '@/assets/email.svg'
+import LeadsSVG from '@/assets/leads.svg'
+import CampaignsSVG from '@/assets/campaigns.svg'
+import router from '@/router'
+import SlackSVG from '@/assets/slack.svg'
+import PlaySVG from '@/assets/play.svg'
 
 const options = [
   {
     label: 'All leads',
     icon: LeadsSVG,
     onClick: () => {
-      console.log('All leads clicked');
-      router.push({ name: 'dashboard' });
+      console.log('All leads clicked')
+      router.push({ name: 'dashboard' })
     },
-    route: 'dashboard',
+    route: 'dashboard'
   },
   {
     label: 'Master Inbox',
     icon: EmailSVG,
-    disabled: true,
+    disabled: true
   },
   {
     label: 'Email Campaigns',
     icon: CampaignsSVG,
     route: 'emails',
     onClick: () => {
-      console.log('Email Campaigns clicked');
-      router.push({ name: 'emails' });
-    },
-  },
+      console.log('Email Campaigns clicked')
+      router.push({ name: 'emails' })
+    }
+  }
 ]
 
 const bottomOptions = [
   {
     label: 'Join Slack Community',
     disabled: true,
-    icon: SlackSVG,
-  }, {
+    icon: SlackSVG
+  },
+  {
     label: 'Smartlead Tutorials',
     icon: PlaySVG,
-    disabled: true,
+    disabled: true
   }
 ]
 </script>
