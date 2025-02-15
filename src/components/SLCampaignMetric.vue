1da52cf993acaec22fa8f2ca75504247aa5cc8a4
Sidhanthsur <sidhanth.surana@aspireapp.com>
2025-02-15 18:57:31 +0530

    feat: update SLCampaignMetric component with new class naming and improved styling

--- src/components/SLCampaignMetric.vue
+++ src/components/SLCampaignMetric.vue
@@ -35,27 +35,27 @@ const icon = computed(() => {
   } else if (props.type === 'positiveReply') {
     return positiveReplySVG
   }
-  return ''
+  return null
 })
 </script>
 
 <template>
-  <div class="campaign-metric">
-    <div class="campaign-metric__content">
+  <div class="sl-campaign-metric">
+    <div class="sl-campaign-metric__content">
       <div>
-        <span :style="{ color }" class="campaign-metric__value">{{ value }}</span>
-        <span v-if="percentage" class="campaign-metric__percentage">{{ percentage }}%</span>
+        <span :style="{ color }" class="sl-campaign-metric__value">{{ value }}</span>
+        <span v-if="percentage" class="sl-campaign-metric__percentage">{{ percentage }}%</span>
       </div>
       <div class="flex items-center">
-        <div class="campaign-metric__label">{{ label }}</div>
-        <img v-if="icon" :src="icon" :alt="label" class="campaign-metric__icon margin-left-2" />
+        <div class="sl-campaign-metric__label">{{ label }}</div>
+        <img v-if="icon" :src="icon" :alt="label" class="sl-campaign-metric__icon margin-left-2" />
       </div>
     </div>
   </div>
 </template>
 
 <style scoped>
-.campaign-metric {
+.sl-campaign-metric {
   display: flex;
   align-items: center;
   background: white;
@@ -63,33 +63,33 @@ const icon = computed(() => {
   margin-right: 2rem;
 }
 
-.campaign-metric__icon {
+.sl-campaign-metric__icon {
   width: 0.75rem;
   height: 0.75rem;
 }
 
-.campaign-metric__content {
+.sl-campaign-metric__content {
   display: flex;
   flex-direction: column;
 }
 
-.campaign-metric__value {
+.sl-campaign-metric__value {
   font-family: DM Sans;
   font-weight: 500;
-  font-size: 20px;
+  font-size: 1.25rem;
 }
 
-.campaign-metric__label {
+.sl-campaign-metric__label {
   font-family: DM Sans;
   font-weight: 400;
-  font-size: 14px;
+  font-size: 0.875rem;
   color: #999ba8;
 }
 
-.campaign-metric__percentage {
+.sl-campaign-metric__percentage {
   font-family: DM Sans;
   font-weight: 400;
-  font-size: 12px;
+  font-size: 0.75rem;
   color: #999ba8;
   margin-left: 0.25rem;
 }
