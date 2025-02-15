7499ee9fd60fa5faee58091eb0f1ba30c2c32852
Sidhanthsur <sidhanth.surana@aspireapp.com>
2025-02-15 10:31:38 +0530

    feat: add campaigns constant with sample data for campaign management

---
+++ src/constants/campaigns.js
@@ -0,0 +1,237 @@
+export const campaigns = [
+  {
+    progress: 35,
+    title: 'SW Zero Personalisation 1',
+    sequences: 5,
+    status: 'DRAFT',
+    statusTime: '13 Apr 2021',
+    metrics: {
+      sent: 0,
+      clicked: {
+        percentage: 17,
+        count: 145
+      },
+      opened: {
+        percentage: 13,
+        count: 233
+      },
+      replied: {
+        percentage: 8,
+        count: 23
+      },
+      positiveReply: {
+        percentage: 2,
+        count: 3
+      }
+    }
+  },
+  //generate random data like the above object with different values
+  {
+    progress: 23,
+    title: 'zero no 2',
+    sequences: 5,
+    status: 'PAUSED',
+    statusTime: '13 Apr 2021',
+    metrics: {
+      sent: 0,
+      clicked: {
+        percentage: 17,
+        count: 145
+      },
+      opened: {
+        percentage: 13,
+        count: 233
+      },
+      replied: {
+        percentage: 8,
+        count: 23
+      },
+      positiveReply: {
+        percentage: 2,
+        count: 3
+      }
+    }
+  },
+  {
+    progress: 18,
+    title: 'SW Zero Personalisation 3',
+    sequences: 5,
+    status: 'STOPPED',
+    statusTime: '13 Apr 2021',
+    metrics: {
+      sent: 0,
+      clicked: {
+        percentage: 17,
+        count: 145
+      },
+      opened: {
+        percentage: 13,
+        count: 233
+      },
+      replied: {
+        percentage: 8,
+        count: 23
+      },
+      positiveReply: {
+        percentage: 2,
+        count: 3
+      }
+    }
+  },
+  {
+    progress: 35,
+    title: 'SW Zero Personalisation 4',
+    sequences: 5,
+    status: 'SENT',
+    statusTime: '13 Apr 2021',
+    metrics: {
+      sent: 0,
+      clicked: {
+        percentage: 17,
+        count: 145
+      },
+      opened: {
+        percentage: 13,
+        count: 233
+      },
+      replied: {
+        percentage: 8,
+        count: 23
+      },
+      positiveReply: {
+        percentage: 2,
+        count: 3
+      }
+    }
+  },
+  {
+    progress: 35,
+    title: 'SW Zero Personalisation 5',
+    sequences: 5,
+    status: 'DRAFT',
+    statusTime: '13 Apr 2021',
+    metrics: {
+      sent: 0,
+      clicked: {
+        percentage: 17,
+        count: 145
+      },
+      opened: {
+        percentage: 13,
+        count: 233
+      },
+      replied: {
+        percentage: 8,
+        count: 23
+      },
+      positiveReply: {
+        percentage: 2,
+        count: 3
+      }
+    }
+  },
+  {
+    progress: 35,
+    title: 'SW Zero Personalisation 6',
+    sequences: 5,
+    status: 'DRAFT',
+    statusTime: '13 Apr 2021',
+    metrics: {
+      sent: 0,
+      clicked: {
+        percentage: 17,
+        count: 145
+      },
+      opened: {
+        percentage: 13,
+        count: 233
+      },
+      replied: {
+        percentage: 8,
+        count: 23
+      },
+      positiveReply: {
+        percentage: 2,
+        count: 3
+      }
+    }
+  },
+  {
+    progress: 35,
+    title: 'SW Zero Personalisation 7',
+    sequences: 5,
+    status: 'DRAFT',
+    statusTime: '13 Apr 2021',
+    metrics: {
+      sent: 0,
+      clicked: {
+        percentage: 17,
+        count: 145
+      },
+      opened: {
+        percentage: 13,
+        count: 233
+      },
+      replied: {
+        percentage: 8,
+        count: 23
+      },
+      positiveReply: {
+        percentage: 2,
+        count: 3
+      }
+    }
+  },
+  {
+    progress: 35,
+    title: 'SW Zero Personalisation 8',
+    sequences: 5,
+    status: 'DRAFT',
+    statusTime: '13 Apr 2021',
+    metrics: {
+      sent: 0,
+      clicked: {
+        percentage: 17,
+        count: 145
+      },
+      opened: {
+        percentage: 13,
+        count: 233
+      },
+      replied: {
+        percentage: 8,
+        count: 23
+      },
+      positiveReply: {
+        percentage: 2,
+        count: 3
+      }
+    }
+  },
+  {
+    progress: 35,
+    title: 'SW Zero Personalisation 9',
+    sequences: 5,
+    status: 'DRAFT',
+    statusTime: '13 Apr 2021',
+    metrics: {
+      sent: 0,
+      clicked: {
+        percentage: 17,
+        count: 145
+      },
+      opened: {
+        percentage: 13,
+        count: 233
+      },
+      replied: {
+        percentage: 8,
+        count: 23
+      },
+      positiveReply: {
+        percentage: 2,
+        count: 3
+      }
+    }
+  }
+]
