7b3117889024be3ad6bf73b3f62b0dc061ec93f8
Sidhanthsur <sidhanth.surana@aspireapp.com>
2025-02-15 22:56:22 +0530

    feat: refactor UserMenuProgress component for improved props handling and accessibility

--- src/components/OfferBanner.vue
+++ src/components/OfferBanner.vue
@@ -11,8 +11,9 @@ const { setOfferVisible } = useUserStore()
     <div class="flex items-center">
       <img :src="offerSVG" alt="offer" />
       <div>
-        Here's a chance to further increase your ROI.
-        <span>Here's a chance to further increase your ROI.</span>
+        <span class="offer-banner__text">Here's a chance to further increase your ROI. </span>
+        <span class="offer-banner__promo-big offer-banner__promo-big--bold">Pay only $540.00</span>
+        <span class="offer-banner__promo-big"> instead of <s>$59000</s></span>
       </div>
     </div>
 
@@ -43,5 +44,27 @@ const { setOfferVisible } = useUserStore()
 
 .offer-banner__button {
   margin-right: 1.875rem;
+}
+
+.offer-banner__text {
+  font-family: DM Sans;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 18.23px;
+  letter-spacing: 0%;
+  color: #00056a;
+  margin-left: 1.375rem;
+}
+
+.offer-banner__promo-big {
+  font-family: DM Sans;
+  font-size: 18px;
+  line-height: 23.44px;
+  text-align: center;
+}
+
+.offer-banner__promo-big--bold {
+  font-weight: 700;
+  color: #d85c7d;
 }
 </style>
